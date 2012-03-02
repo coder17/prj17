4483849492780bfbade21ac2155fe459d8076d84
Schelakov Denis <SchelakovDA@interunis.ru>
2012-03-02 12:07:09 +0400

    add lst files

--- Makefile
+++ Makefile
@@ -3,11 +3,14 @@ LINK			= g++ -s -o prog
 
 OBJ = \
 	main.o \
-	alg8.o
+	alg8.o \
+	lst.o
 
 prog: $(OBJ)
 	$(LINK) $(OBJ)
 
 alg8.o: alg8.cc alg8.h
-main.o: main.cc alg8.h
+lst.o: lst.cc lst.h
+main.o: main.cc alg8.h lst.h
+
 
--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "alg8.h"
+#include "lst.h"
 
 
 int main(int argc, char **argv)
@@ -10,6 +11,17 @@ int main(int argc, char **argv)
 	print_man(n, 0, "nation1");
 
 	kill_nation(n);
+
+	PUPIL_LIST *pl = create_pupil_list();
+	add_pupil(pl, "Sam", 11, 435);
+	add_pupil(pl, "Tom", 13, 6456);
+	add_pupil(pl, "Max", 10, 543);
+	add_pupil(pl, "Sid", 15, 866);
+	add_pupil(pl, "Jeremy", 14, 34);
+	add_pupil(pl, "Vanessa", 8, 423);
+	add_pupil(pl, "Kate", 9, 7665);
+	print_all_pupils(pl);
+	delete_pupil_list(pl);
 
 	return 0;
 }
