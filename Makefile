a95c4a3a116d0a98c47dc2e4a290c403b2be936f
Schelakov Denis <SchelakovDA@interunis.ru>
2012-03-02 14:16:29 +0400

    delete alg8 lst

--- Makefile
+++ Makefile
@@ -3,15 +3,11 @@ LINK			= g++ -s -o prog
 
 OBJ = \
 	main.o \
-	alg8.o \
-	lst.o \
-	gr.o
+	pit.o
 
 prog: $(OBJ)
 	$(LINK) $(OBJ)
 
-alg8.o: alg8.cc alg8.h
-lst.o: lst.cc lst.h
-gr.o: gr.cc gr.h
-main.o: main.cc alg8.h lst.h gr.h
+pit.o: pit.cc pit.h
+main.o: main.cc pit.h
 
--- main.cc
+++ main.cc
@@ -1,32 +1,10 @@
 #include <stdio.h>
-#include "alg8.h"
-#include "lst.h"
-#include "gr.h"
+#include "pit.h"
 
 
 int main(int argc, char **argv)
 {
-	NATION *n = create_nation(5);
-
-	set_man(n, 0, "Thomas", 19, 1, 1934);
-	print_man(n, 0, "nation1");
-
-	kill_nation(n);
-
-	PUPIL_LIST *pl = create_pupil_list();
-	add_pupil(pl, "Sam", 11, 435);
-	add_pupil(pl, "Tom", 13, 6456);
-	add_pupil(pl, "Max", 10, 543);
-	add_pupil(pl, "Sid", 15, 866);
-	add_pupil(pl, "Jeremy", 14, 34);
-	add_pupil(pl, "Vanessa", 8, 423);
-	add_pupil(pl, "Kate", 9, 7665);
-	print_all_pupils(pl);
-	delete_pupil_list(pl);
-
-	draw_line(5, 5, 20);
-	draw_rect(3, 10, 7, 12);
-	draw_square(3, 10, 20);
+	func_m();
 
 	return 0;
 }
